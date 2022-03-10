75e889fcb20c6b256b42cfab4c6df9c0093a9eee
Thabata-Orbeteli <thabata.orbeteli@gmail.com>
2022-03-10 16:23:47 -0300

    10-03

--- src/main/kotlin/Hamburgueria/Pedido.kt
+++ src/main/kotlin/Hamburgueria/Pedido.kt
@@ -2,107 +2,114 @@ package Hamburgueria
 
 open class Pedido{
 
-    fun pedidoCliente() {
-        println("MENU")
-        println("Escolha uma opção deliciosa abaixo:")
-        println("1 - Combo Salada (x-salada,batata frita,refrigerante lata) ")
-        println("2 - Combo Chicken (steak de frango,batata frita,refrigerante lata)")
-        println("3 - Combo Tudão (x-tudo,batata frita,refrigerante lata)")
-        println("4 - Combo Cheddar (hambúrger,batata frita com cheddar,refrigerante lata)")
-        println("5 - Combo Veggie (hambúrguer de soja, batata frita,refrigerante lata")
-        println("6 - Sair")
-        var opcao = readLine()!!.toInt()
+    companion object {
 
-        if (opcao < 1 || opcao > 6) {
-            println("\nOPÇÃO INVALIDA!!!!\n")
+        fun pedidoCliente(cadastro: Cadastro) {
 
-        } else {
-
-            when (opcao) {
-
-                1 -> {
-                    println("Você escolheu o Combo Salada")
-                    pagamentoCliente()
-                }
-                2 -> {
-                    println("Você escolheu o Combo Chicken")
-                    pagamentoCliente()
-                }
-                3 -> {
-                    println("Você escolheu o Combo Tudão")
-                    pagamentoCliente()
-                }
-                4 -> {
-                    println("Você escolheu o Combo Tudão")
-                    pagamentoCliente()
-                }
-                5 -> {
-                    println("Você escolheu o Combo Veggie")
-                    pagamentoCliente()
-                }
-                6 -> println("Sair")
+            if(cadastro.primeiraVez){
+                println("Essa é sua primeira compra!")
+                println("Você ganhou um Cupcake!")
+                cadastro.primeiraVezCliente()
             }
 
-            while(opcao ==6){
+            println("MENU")
+            println("Escolha uma opção deliciosa abaixo:")
+            println("1 - Combo Salada (x-salada,batata frita,refrigerante lata) ")
+            println("2 - Combo Chicken (steak de frango,batata frita,refrigerante lata)")
+            println("3 - Combo Tudão (x-tudo,batata frita,refrigerante lata)")
+            println("4 - Combo Cheddar (hambúrger,batata frita com cheddar,refrigerante lata)")
+            println("5 - Combo Veggie (hambúrguer de soja, batata frita,refrigerante lata")
+            println("6 - Sair")
+            var opcao = readLine()!!.toInt()
+
+            if (opcao < 1 || opcao > 6) {
+                println("\nOPÇÃO INVALIDA!!!!\n")
+
+            } else {
+
+                when (opcao) {
+
+                    1 -> {
+                        println("Você escolheu o Combo Salada")
+                        pagamentoCliente()
+                    }
+                    2 -> {
+                        println("Você escolheu o Combo Chicken")
+                        pagamentoCliente()
+                    }
+                    3 -> {
+                        println("Você escolheu o Combo Tudão")
+                        pagamentoCliente()
+                    }
+                    4 -> {
+                        println("Você escolheu o Combo Tudão")
+                        pagamentoCliente()
+                    }
+                    5 -> {
+                        println("Você escolheu o Combo Veggie")
+                        pagamentoCliente()
+                    }
+                    6 -> println("Sair")
+                }
+
+                while (opcao == 6) {
+                    println("Obrigado!")
+                    break
+                }
+            }
+        }
+
+        private fun pagamentoCliente() {
+            println("Escolha uma opção de pagamento:")
+            println("1 - Dinheiro")
+            println("2 - Cartão ou Pix")
+            println("3 - Cancelar")
+            var opcao = readLine()!!.toInt()
+
+            if (opcao < 1 || opcao > 6) {
+                println("\nOPÇÃO INVALIDA!!!!\n")
+
+            } else {
+
+                when (opcao) {
+                    1 -> troco()
+                    2 -> cartao()
+                    3 -> cancelar()
+                }
+            }
+        }
+
+        private fun sair() {
             println("Obrigado!")
-            break
+
         }
+
+        private fun cancelar() {
+            println("Pedido Cancelado!")
         }
-    }
 
-    fun pagamentoCliente() {
-        println("Escolha uma opção de pagamento:")
-        println("1 - Dinheiro")
-        println("2 - Cartão ou Pix")
-        println("3 - Cancelar")
-        var opcao = readLine()!!.toInt()
+        private fun troco() {
+            println("Você vai precisar de troco?")
+            println("Informe a opção: ")
+            println("1 - Sim")
+            println("2 - Não")
+            var opc = readLine()!!.toInt()
 
-        if (opcao < 1 || opcao > 6) {
-            println("\nOPÇÃO INVALIDA!!!!\n")
-
-        } else {
-
-            when (opcao) {
-                1 -> troco()
-                2 -> cartao()
-                3 -> cancelar()
+            when (opc) {
+                1 -> println("Sim")
+                2 -> println("Não")
             }
+            println("Pedido recebido!")
+            println("Lembre-se de pagar na entrega!")
+
         }
-    }
 
-
-    fun sair() {
-        println("Obrigado!")
-
-    }
-
-    fun cancelar() {
-        println("Pedido Cancelado!")
-    }
-
-    fun troco() {
-        println("Você vai precisar de troco?")
-        println("Informe a opção: ")
-        println("1 - Sim")
-        println("2 - Não")
-        var opc = readLine()!!.toInt()
-
-        when (opc) {
-            1 -> println("Sim")
-            2 -> println("Não")
+        private fun cartao() {
+            println("Pedido recebido!")
+            println("Lembre-se de pagar na entrega!")
         }
-        println("Hamburgueria.Pedido recebido!")
-        println("Lembre de pagar na entrega!")
 
     }
-
-    fun cartao() {
-        println("Hamburgueria.Pedido recebido!")
-        println("Lembre de pagar na entrega!")
-    }
-
-
-
 
 }
 
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,24 +1,25 @@
-import Hamburgueria.ClienteCadastro
-import Hamburgueria.Cliente1
-import Hamburgueria.Pedido
+import Hamburgueria.*
 
 fun main() {
 
     println("Bem -vindo a Hambúrgueria DEV BURGUERS!")
-    var etapa1 = ClienteCadastro()
+    println("Digite seu nome: ")
+    var nome=readLine()!!
+    println("Digite seu CPF: ")
+    var cpf=readLine()!!
+    println("Digite seu endereço: ")
+    var endereco=readLine()!!
+    println("Digite seu telefone: ")
+    var telefone=readLine()!!
+    println("Digite seu e-mail: ")
+    var email=readLine()!!
+
+
+    var etapa1 = Cadastro(nome,cpf,endereco,telefone,email)
+
     println("\n")
 
-    etapa1.nomeCliente()
-    etapa1.telefoneCliente()
-    etapa1.emailCliente()
-    etapa1.enderecoCliente()
+    Pedido.pedidoCliente(etapa1)
 
-    var etapa2=Cliente1()
-    etapa2.exclusivo()
-
-    println("\n")
-    var etapa3 = Pedido()
-    etapa3.pedidoCliente()
-    println("\n")
 
 }
